ca9b8168113df34c5cf56afcb6751143fe2c524c
Ruben De Swaef <rubendeswaef@gmail.com>
2018-10-18 16:19:04 +0200

    First

---
+++ src/main/java/be/uantwerpen/clubiot/ClubiotApplication.java
@@ -0,0 +1,12 @@
+package be.uantwerpen.clubiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubiotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubiotApplication.class, args);
+    }
+}
---
+++ src/test/java/be/uantwerpen/clubiot/ClubiotApplicationTests.java
@@ -0,0 +1,16 @@
+package be.uantwerpen.clubiot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClubiotApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
