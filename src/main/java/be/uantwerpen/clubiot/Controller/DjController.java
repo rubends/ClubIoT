e78a791e032062e6069f3eaf7d05f4c55f34e536
Ruben De Swaef <rubendeswaef@gmail.com>
2018-10-18 17:00:40 +0200

    update api

--- src/main/java/be/uantwerpen/clubiot/Controller/DjController.java
+++ src/main/java/be/uantwerpen/clubiot/Controller/DjController.java
@@ -1,4 +1,26 @@
 package be.uantwerpen.clubiot.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+@Controller // spring --> controller class --> autowired objecten
 public class DjController {
+    @RequestMapping({"/","/dj"})
+    @ResponseBody
+    public String showHomepage(){ return "Hello World"; }
+
+    @RequestMapping(value="/api/search", method= RequestMethod.GET)
+    public String searchSong(@RequestParam("song") String song)
+    {
+        return song;
+    }
+
+    @RequestMapping(value="/api/play/{id}", method= RequestMethod.POST)
+    public String playSong(@PathVariable String id)
+    {
+        return id;
+    }
+
 }
