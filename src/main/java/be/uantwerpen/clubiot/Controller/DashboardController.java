676ae2b3c13b524698573be91eb98a298e26fd47
VadimGouskov <vadimgouskov@hotmail.com>
2018-10-22 08:16:15 +0200

    Dashboard <-> VotesDummy Model test
    - added
    --VotesDummy (Model): Serve dummy data to Dashboard.html
    - changed
    --Dashboard.html: now renders and accepts data from VotesDummt
    -- DashboardController: serve dashboard.html

--- src/main/java/be/uantwerpen/clubiot/Controller/DashboardController.java
+++ src/main/java/be/uantwerpen/clubiot/Controller/DashboardController.java
@@ -1,6 +1,8 @@
 package be.uantwerpen.clubiot.Controller;
 
+import be.uantwerpen.clubiot.Model.VotesDummy;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,12 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DashboardController {
     @RequestMapping({"/dashboard"})
-    @ResponseBody
-    public String showDashboardPage(){
-        return "Hello Dashboard";
+    public String showDashboard(ModelMap model){
+        VotesDummy votes = new VotesDummy();
+        model.addAttribute("votes", votes);
+        return "dashboard";
     }
 
-    @RequestMapping(value="/api/songs/{id}", method= RequestMethod.POST)
+    @RequestMapping(value="/api/songs/{id}", method= RequestMethod.GET)
     public String playSong(@PathVariable int id){
       return "song: " + id;
     }
