121223660a9d30b20bc1aff05451adfe42e27d4c
ThomasCassimon <thomas.cassimon@outlook.com>
2018-10-25 17:56:21 +0200

    Bugfix in application properties that caused the application to connect to the wrong database.

--- src/main/java/be/uantwerpen/clubiot/Service/NoSQLService.java
+++ src/main/java/be/uantwerpen/clubiot/Service/NoSQLService.java
@@ -15,6 +15,7 @@ public class NoSQLService
 
     private final MongoDbFactory mongo;
 
+    @Autowired
     public NoSQLService(MongoDbFactory mongo)
     {
         this.mongo = mongo;
@@ -52,26 +53,41 @@ public class NoSQLService
     public int getVotes (long songId)
     {
         DB database = this.mongo.getDb();
+
+        System.out.println("Database: " + database.getName());
+
+        for (String collection: database.getCollectionNames())
+        {
+            System.out.println("Collection: " + collection);
+        }
+
         DBCollection voteCache = database.getCollection("vote_cache");
 
-        DBObject query = new BasicDBObject("_id", new BasicDBObject("eq", songId));
+        DBObject query = new BasicDBObject("_id", songId);
 
-        DBCursor voteCacheIt = voteCache.find(query);
+        DBCursor voteCacheIt = voteCache.find(/*query*/);
 
+        System.out.println("vote_cache: " + voteCache.count());
+
+        /*
         if (voteCacheIt.count() == 0)
         {
             System.err.println("Song ID \"" + songId + "\" wasn't present in NoSQL Database.");
             return 0;
         }
-
+        */
         int numVotes = 0;
 
         while (voteCacheIt.hasNext())
         {
-            DBObject object =  voteCacheIt.next();
+            DBObject object = voteCacheIt.next();
+
+            System.out.println(object.toString());
 
             numVotes += (Integer)object.get("value");
         }
+
+        voteCacheIt.close();
 
 
         return numVotes;
