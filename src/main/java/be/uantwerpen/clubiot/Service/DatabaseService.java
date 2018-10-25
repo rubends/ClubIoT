aa17a9a6178eb195d6b2786a72e40e14d5294ef6
Ruben De Swaef <rubendeswaef@gmail.com>
2018-10-25 15:59:13 +0200

    fix broker

--- src/main/java/be/uantwerpen/clubiot/Service/DatabaseService.java
+++ src/main/java/be/uantwerpen/clubiot/Service/DatabaseService.java
@@ -10,12 +10,6 @@ public class DatabaseService {
     @Autowired
     private MusicRepository musicRepository;
 
-    //TODO This is only for testing
-    public void addSongToRepo(String title, String artist, int year)
-    {
-        musicRepository.save(new Music(title,artist,year));
-    }
-
     public Iterable<Music> findAll() {
         return this.musicRepository.findAll();
     }
