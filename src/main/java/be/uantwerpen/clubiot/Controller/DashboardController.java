34563e9f117b47b39d226c844058f8e70b02ed9a
Ruben De Swaef <rubendeswaef@gmail.com>
2018-10-25 16:35:14 +0200

    backend search song on artist

--- src/main/java/be/uantwerpen/clubiot/Controller/DashboardController.java
+++ src/main/java/be/uantwerpen/clubiot/Controller/DashboardController.java
@@ -16,37 +16,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DashboardController {
 
-    private BrokerService brokerService;
-    private DatabaseService databaseService;
-
     @RequestMapping({"/dashboard"})
     public String showDashboard(ModelMap model){
-        initMqttBroker();
         VotesDummy votes = new VotesDummy();
         model.addAttribute("votes", votes);
         return "dashboard";
-    }
-
-    @RequestMapping(value="/api/songs/{id}", method= RequestMethod.GET)
-    public String playSong(@PathVariable int id){
-      return "song: " + id;
-    }
-
-    public void initMqttBroker(){
-
-        JSONObject musicObject = new JSONObject();
-        musicObject.put("title","Spring Nicht");
-        musicObject.put("artist","Tokio Hotel");
-        musicObject.put("year",1906);
-        musicObject.put("songid",358666);
-
-
-        brokerService = new BrokerService();
-        // IN CONSTRUCTOR
-       // brokerService.connect("tcp://143.129.39.126:1883", "dj_web", "a134bie5"); // open connection: "tcp://iot.eclipse.org:1883"
-       // brokerService.subscribe("music");
-//        brokerService.publishString("music", "Hello");
-        brokerService.publishJson("music", musicObject);
-
     }
 }
